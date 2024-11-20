9d57fd25c10907e82e6a1e95eeb48217106f226e
giladmev <giladmgm@gmail.com>
2024-11-20 23:40:28 +0200

    add code files

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ from generate_assembly_report import generate_assembly_report
 
 
 if __name__ == "__main__":
-    # fasta_file = "phiX_genome.fasta"
-    # read_length = 100
-    # num_reads = 10000
-    # min_overlap = 20
-    # error_prob = 0.01
-    #
-    # # Generate error-free reads
-    # error_free_reads_file = generate_reads(fasta_file, read_length, num_reads)
-    #
-    # # Generate error-prone reads
-    # error_prone_reads_file = generate_reads(fasta_file, read_length, num_reads, error_prob=error_prob)
-    #
-    # # Assemble the reads
-    # contigs_error_free = assemble_reads(error_free_reads_file, min_overlap)
-    # contigs_error_prone = assemble_reads(error_prone_reads_file, min_overlap)
+    fasta_file = "phiX_genome.fasta"
+    read_length = 100
+    num_reads = 10000
+    min_overlap = 20
+    error_prob = 0.01
+
+    # Generate error-free reads
+    error_free_reads_file = generate_reads(fasta_file, read_length, num_reads)
+
+    # Generate error-prone reads
+    error_prone_reads_file = generate_reads(fasta_file, read_length, num_reads, error_prob=error_prob)
+
+    # Assemble the reads
+    contigs_error_free = assemble_reads(error_free_reads_file, min_overlap)
+    contigs_error_prone = assemble_reads(error_prone_reads_file, min_overlap)
     generate_assembly_report("assembled_contigs_error_free_reads.fasta", "assembled_contigs_error_prone_reads.fasta", "phiX_genome.fasta")
